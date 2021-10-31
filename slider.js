e1ba3fe6482091f973cc651c9ea5e31339ae3216
DezzysteR <93470091+DezzysteR@users.noreply.github.com>
2021-10-31 12:45:51 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,35 @@
+const slider = $(".feedback__list").bxSlider({
+pager: false,
+controls: false,
+infiniteLoop: true,
+touchEnabled: true,
+hideControlOnEnd: false,
+});
+
+$(".button--prev").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
+
+$(".button--next").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
+
+const portfolio = $(".portfolio__list").bxSdider({
+    pager: false,
+controls: false,
+infiniteLoop: true,
+touchEnabled: true,
+hediControlOnEnd: false,
+});
+
+$("button--portfolio-prev").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
+
+$("button--portfolio-prev").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
